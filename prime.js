8a5a01fd258454d4b1e6e5c9314292e86e6a0c9a
shivamvr <shivamweb2@gmail.com>
2023-01-08 19:24:46 +0530

    code fixed

--- prime.js
+++ prime.js
@@ -3,7 +3,7 @@ function prime(n){
     if(n<=1){
      return false
     }
-    for(var i=2; i<n**0.5; i++){
+    for(var i=2; i<=n**0.5; i++){
         if(n%i==0){
             return false
         }
